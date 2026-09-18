9a13e9cdf106d221080b0ef2affb29458767039b
Siarhei Nestsiaronak <nesterka375@gmail.com>
2026-09-18 13:47:33 +0200

    add migrations, create database and rename in settings

--- press_agency/settings.py
+++ press_agency/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = "press_agency.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "NAME": BASE_DIR / "catalog.sqlite3",
     }
 }
 
