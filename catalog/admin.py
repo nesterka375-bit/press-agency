2f40d4dea04ac81cd9f1c8bbd176d1e536be221e
Siarhei Nestsiaronak <nesterka375@gmail.com>
2026-09-18 14:57:04 +0200

    editing models and add AdminTopic & AdminRedactor

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import Topic, Redactor
+
+
+@admin.register(Topic)
+class TopicAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
+
+
+@admin.register(Redactor)
+class RedactorAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ("years_of_experience",)
+    search_fields = (
+        "first_name",
+        "last_name",
+        "username",
+    )
+    list_filter = ("years_of_experience",)
+    fieldsets = UserAdmin.fieldsets + ((None, {"fields": ("years_of_experience",)}),)
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {"classes": ("wide",), "fields": ("years_of_experience",)}),
+    )
--- catalog/models.py
+++ catalog/models.py
@@ -5,12 +5,18 @@ from django.db import models
 class Topic(models.Model):
     name = models.CharField(max_length=255)
 
+    class Meta:
+        ordering = ("name",)
+
     def __str__(self):
         return self.name
 
 
 class Redactor(AbstractUser):
     years_of_experience = models.IntegerField(null=True, blank=True, default=0)
+
+    class Meta:
+        ordering = ("first_name",)
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}. Years of Experience: {self.years_of_experience}"
@@ -24,6 +30,12 @@ class Newspaper(models.Model):
         Topic, on_delete=models.CASCADE, related_name="newspapers"
     )
     publishers = models.ManyToManyField(Redactor, blank=True, related_name="newspapers")
+
+    class Meta:
+        ordering = (
+            "-published_date",
+            "name",
+        )
 
     def __str__(self):
         return f"{self.name}. Topic: {self.topic.name}. Date: {self.published_date}"
